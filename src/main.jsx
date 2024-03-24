fa465e0ba8cc690ff2548af529a8b3cb5b8f1aba
Jhonnatan Espinoza Rojas <hytzy18@gmail.com>
2024-03-23 23:37:38 -0500

    new banner - deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Join } from '@/pages/Join';
 import { Home } from '@/pages/Home';
-import { Root } from '@/pages/v1/Root';
 
 import './index.css';
 
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Root />,
-	},
   {
-    path: 'demo',
+    path: '/',
     element: <Home />,
   },
 	{
